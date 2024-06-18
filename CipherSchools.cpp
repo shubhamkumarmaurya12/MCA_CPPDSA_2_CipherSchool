da4a7c6201863ce5dbb01dc9bedcd8f65649d3d9
shubhamkumarmaurya12 <shubhamkumarmaurya267@gmail.com>
2024-06-18 14:03:46 +0530

    virtual function

--- CipherSchools.cpp
+++ CipherSchools.cpp
@@ -1,6 +1,28 @@
-#include<iostream>
+/*
+    pollymorphism
+   >>   when same entity behave different in different situation that is calld polymorphism
+*/
+#include<bits/stdc++.h>
 using namespace std;
-int main()
+class Base {
+    public:
+  virtual  void show(){
+    cout<<"this is base class"<<endl;
+    }
+};
+
+class Derived : public Base{
+    public:
+   void show(){
+    cout<<"this is derived class"<<endl;
+   }
+};
+int main() 
 {
-    cout<<"hello world";
+    Derived d;
+
+    Base *ptr = &d;
+    ptr->show();
+    
+    return 0;
 }
