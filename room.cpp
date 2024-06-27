1a6282decf1c6e9041e2256da1df9eafc3f6e82a
shubhamkumarmaurya12 <shubhamkumarmaurya267@gmail.com>
2024-06-27 15:33:34 +0530

    2D array wave printing

--- room.cpp
+++ room.cpp
@@ -1,26 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-void Sort(int arr[], int n){
  
-   for(int i=0; i<n-1; i++){
-      int min = i;
-      for(int j=i+1; j<n; j++){
-         if(arr[min]>arr[j]){
-            min=j;
-         }
-      }
-      swap(arr[i],arr[min]);
-   }
-for(int i=0; i<n; i++){
-   cout<<arr[i]<<" ";
-}
-}
+ int main(){
+   string str="Shubham kumar maurya";
+    int n = str.length();
+       int i=0;
+        int j=n-1;
+        while(j>i){
+         swap(str[i],str[j]);
+         i++;
+         j--;
+        }
+    
 
-int main(){
-
-  int arr[] = {8,7,2,4,9};
-  Sort(arr,5);
+    cout<<str;
 
    return 0;
-}
+ }
