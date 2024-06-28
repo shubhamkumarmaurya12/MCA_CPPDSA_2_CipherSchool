550f73b658fd9711f08a9454f653275c903b4189
shubhamkumarmaurya12 <shubhamkumarmaurya267@gmail.com>
2024-06-28 15:07:29 +0530

    fibonacci

--- dynamic/2d_array.cpp
+++ dynamic/2d_array.cpp
@@ -2,30 +2,30 @@
 using namespace std;
 
 int main(){
-   int n;
-   cin>>n;
+   int row,col;
+   cin>>row>>col;
 
-   int **arr = new int*[n];
+   int **arr = new int*[row];
 
-   for(int i=0; i<n; i++){
+   for(int i=0; i<row; i++){
     arr[i] = new int[i];
    }
 
 
-   for(int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
+   for(int i=0; i<row; i++){
+    for(int j=0; j<col; j++){
         cin>>arr[i][j];
     }
    }
 
-    for(int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
+    for(int i=0; i<row; i++){
+    for(int j=0; j<col; j++){
         cout<<arr[i][j]<<" ";
     }
  cout<<endl;
    }
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<row; i++){
         delete []arr[i];
     }
 
