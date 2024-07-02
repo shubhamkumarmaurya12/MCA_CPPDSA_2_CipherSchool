5ea8a5c93d27e295295aa34e4c250c62fae409fd
shubhamkumarmaurya12 <shubhamkumarmaurya267@gmail.com>
2024-07-02 19:47:03 +0530

    All operation of Singly linked list

--- Doubly_linkedlist.cpp
+++ Doubly_linkedlist.cpp
@@ -14,21 +14,34 @@ class Node{
 void Insert(Node* &head, int d){
     if(head==NULL){
         head=new Node(d);
-    }
+    } else {
     Node *temp = new Node(d);
     temp->next = head;
     head->prev = temp;
     head=temp;
+    }
 }
 
 void Display(Node *head){
     Node  *temp = head;
+    while (temp->next != NULL) {
+      temp = temp->next;
+    }
     while(temp!=NULL){
         cout<<temp->data<<" ";
-        temp=temp->next;
+        temp=temp->prev;
     }
 }
-
+bool Search(Node* head, int key){
+     Node *temp = head;
+     while(temp!=NULL){
+        if(temp->data==key){
+               return true;
+        }
+        temp=temp->next;
+     }
+     return false;
+}
 int main(){
 
 Node *head = NULL;
@@ -40,5 +53,7 @@ Insert(head,17);
 Insert(head,9);
 
 Display(head);
+
+cout<<endl<<Search(head,18);
     return 0;
 }
--- Single_Linked_List.cpp
+++ Single_Linked_List.cpp
@@ -23,6 +23,7 @@
    tail=tail->next;
   }
 
+
  
  }
 
@@ -34,6 +35,7 @@ void print(Node *head){
     }
 }
 
+// Searching a specific element
 bool Search(Node *head,int key){
   Node *temp = head;
   while(temp!=NULL){
@@ -46,6 +48,61 @@ bool Search(Node *head,int key){
   return false;
 }
 
+void reverse(Node* &head){
+Node* prev = NULL;
+Node* curr = head;
+Node* next;
+  while(curr!=NULL){
+    next=curr->next;
+    curr->next=prev;
+    prev=curr;
+    curr=next;
+  }
+  head=prev;
+}
+
+// Insert At Any Position
+void InsertATAny(Node* &head,int data, int pos){
+pos--;
+Node *temp = head;
+while(pos--){
+temp=temp->next;
+}
+
+Node *temp2 = new Node(data);
+temp2->next = temp->next;
+temp->next=temp2;
+
+}
+
+//Delete first element
+void Delete(Node* &head){
+  if(head==NULL){
+    return;
+  }
+
+  Node *temp = head;
+  head=head->next;
+  delete temp;
+}
+
+// Delete last element
+void Delete2(Node* &head){
+  Node *temp = head;
+  if(temp->next==NULL){
+      delete temp;
+      head=NULL;
+  }
+
+  Node *curr = head;
+  Node *prev = NULL;
+  while(curr->next!=NULL){
+    prev=curr;
+    curr=curr->next;
+  }
+  prev->next=curr->next;
+  delete curr;
+}
 
 int main(){
 
@@ -56,7 +113,15 @@ InsertAtTail(head,tail,21);
 InsertAtTail(head,tail,9); 
 InsertAtTail(head,tail,6); 
 
+//print(head);
+ 
+// cout<<Search(head,21);
+
+//reverse(head);
+
+//InsertATAny(head,20,3);
+Delete(head);
+Delete2(head);
 print(head);
-//cout<<Search(head,21);
     return 0;
 }
